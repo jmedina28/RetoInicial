af429baa9d4f56c1ee7295be1f72c9a00c37eb67
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-09 17:22:26 +0200

    reestructuración

--- pruebasvarias.py
+++ pruebasvarias.py
@@ -1,12 +1,5 @@
 lista= [[6,8],[7,9][4,8],[0,3,9],[],[0,1,7],[2,6],[1,3],[2,4],[4,6]]
-movimientos=[]
 
-def movimientos(inicio, recorrido):
-    n = 0
-    if recorrido:
-        for i in lista[inicio]:
-            movimiento += 1 + movimientos(n, recorrido - 1)
-    return movimiento
 
 
 
