cda981fb697953f557bed299f23f4cd749194c15
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-09-09 16:45:19 +0200

    pruebas varias

---
+++ pruebasvarias.py
@@ -0,0 +1,20 @@
+lista1= [6,8]
+lista2= [7,9]
+lista3= [4,8]
+lista4= [3,9]
+lista6= [1,7]
+lista7= [2,6]
+lista8= [1,3]
+lista9= [2,4]
+lista0= [4,6]
+movimientos=[]
+
+def movimiento1():
+    mov=int(input(("Cuántos movimientos quieres hacer")))
+    if mov == 1:
+        movimientofinal = len(lista1) + len(lista2)+ len(lista3) + len(lista4) + len(lista6) + len(lista7) + len(lista8) + len(lista9)
+
+
+
+
+
