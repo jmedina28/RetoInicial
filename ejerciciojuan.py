95b262c8e601e5bd0b513c0163e071cd5f728f10
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-09-09 17:22:46 +0200

    Update ejerciciojuan.py

--- ejerciciojuan.py
+++ ejerciciojuan.py
@@ -13,20 +13,26 @@ def calculos():
     tablero = teclado(n)
     for i in range(0, n):
         for j in range(0, n):
+            a = 0 + int(i)
+            b = 0 + int(j)
             tablero[i][j] = "r"
             for x in range(0, n):
                 if tablero[i][x] != "r":
                     tablero[i][x] = "x"
                 if tablero[x][i] != "r":
                     tablero[x][i] = "x"
-            while i != 0 or j != 0:
-                i-=1
-                j-=1
-                tablero[i][j] = "x"
-            while i != n-1 or j != n-1:
-                i+=1
-                j+=1
-                tablero[i][j] = "x"
+            if a!=0 and b!=0:
+                while a != 0 or b != 0:
+                    a-=1
+                    b-=1
+                    tablero[a][b] = "x"
+            a = 0 + int(i)
+            b = 0 + int(j)
+            if a!=n-1 and b!=n-1:
+                while a != n-1 and b != n-1:
+                    a+=1
+                    b+=1
+                    tablero[a][b] = "x"
             print(tablero)
             tablero = teclado(n)
 
