75ddf709f0cdb10b73e940930f496ecafd624223
jmedina28 <juanperezmedina67@gmail.com>
2022-09-10 14:36:00 +0200

    mejoras

--- prueba.py
+++ prueba.py
@@ -13,41 +13,66 @@ diccionario= {
         0: [4,6]
     }
 
-casilla = 1
+casilla = diccionario[4]
 
 def recorrer1():
+    global posibilidad1
     posibilidad1 = []
-
-    
-    for i in range(len(diccionario[casilla])):
+    for i in range(len(diccionario[4])):
         posicion = i
-        posibilidad1.append(diccionario[casilla][posicion])
-    print(posibilidad1)
-    posibilidades_unicas = []
-    for o in posibilidad1:
-        if o not in posibilidades_unicas:
-            posibilidades_unicas.append(o)
+        posibilidad1.append(diccionario[4][posicion])
+    return posibilidad1
 
-    print(posibilidades_unicas)
-    
-    for n in range(len(posibilidades_unicas)):
-        posibilidad1.extend(diccionario[posibilidades_unicas[n]])
-        
-    print(posibilidad1)
-    for o in posibilidad1:
-        if o not in posibilidades_unicas:
-            posibilidades_unicas.append(o)
-    print(posibilidades_unicas)
-
-    print(len(posibilidad1)-len(diccionario[casilla]))
-    
 recorrer1()
+print(posibilidad1)
 
 
-    
-   
-    
+def recorrer2():
+    global posibilidad2
+    posibilidad2 = []
+    for i in range(len(posibilidad1)):
+        posicion = i
+        posibilidad2.extend(diccionario[posibilidad1[posicion]])
+    return posibilidad2
+print(recorrer2())
 
-
-    
-
+def recorrer3():
+    global posibilidad3
+    posibilidad3 = []
+    for i in range(len(posibilidad2)):
+        posicion = i
+        posibilidad3.extend(diccionario[posibilidad2[posicion]])
+    return posibilidad3
+print(recorrer3()), print(len(posibilidad3))
+def recorrer4():
+    global posibilidad4
+    posibilidad4 = []
+    for i in range(len(posibilidad3)):
+        posicion = i
+        posibilidad4.extend(diccionario[posibilidad3[posicion]])
+    return posibilidad4
+print(recorrer4()), print(len(posibilidad4))
+def recorrer5():
+    global posibilidad5
+    posibilidad5 = []
+    for i in range(len(posibilidad4)):
+        posicion = i
+        posibilidad5.extend(diccionario[posibilidad4[posicion]])
+    return posibilidad5
+print(recorrer5()), print(len(posibilidad5))
+def recorrer6():
+    global posibilidad6
+    posibilidad6 = []
+    for i in range(len(posibilidad5)):
+        posicion = i
+        posibilidad6.extend(diccionario[posibilidad5[posicion]])
+    return posibilidad6
+print(recorrer6()), print(len(posibilidad6))
+def recorrer7():
+    global posibilidad7
+    posibilidad7 = []
+    for i in range(len(posibilidad6)):
+        posicion = i
+        posibilidad7.extend(diccionario[posibilidad6[posicion]])
+    return posibilidad7
+print(recorrer7()), print(len(posibilidad7))
