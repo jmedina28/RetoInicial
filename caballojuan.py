0d7138a711c1a7fac715fa9034fceb0a3e76aa6e
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-14 12:59:21 +0200

    recursividad

--- caballojuan.py
+++ caballojuan.py
@@ -1,15 +1,15 @@
 # Autor: Juan Antonio Pérez Medina.
 
-x, y, z, j, movimientos = [2], [3], [2], [2], int(input("Introduce el número de movimientos que desea que incluya sus combinaciones: "))
+x, y, z, j, movimientos, movimiento = [2], [3], [2], [2], int(input("Introduce el número de movimientos que desea que incluya sus combinaciones: ")), 0
 
-def recorrer(nmov):
-
-    for i in range(nmov-1):
-        if i % 2 == 0: 
-            y.append(y[i]*2), z.append(z[i]*2), x.append(x[i]+y[i]), j.append(y[i+1]) 
+def recorrer():
+    if movimiento < movimientos-2:
+        if movimiento % 2 == 0: 
+            y.append(y[movimiento]*2), z.append(z[movimiento]*2), x.append(x[movimiento]+y[movimiento]), j.append(y[movimiento+1]) 
         else:
-            y.append(y[i]*2 + z[i]), z.append(y[i] + z[i]), x.append(x[i]*2), j.append(j[i]*2)
-
-recorrer(movimientos)
-print(x,y,z,j)
-print("La cantidad de combinaciones posibles de " + str(movimientos) + " es de " + str(x[-1]*4+y[-1]*2+z[-1]*2+j[-1]))
+            y.append(y[movimiento]*2 + z[movimiento]), z.append(y[movimiento] + z[movimiento]), x.append(x[movimiento]*2), j.append(j[movimiento]*2) 
+        movimiento += 1, recorrer()
+    else:
+        print(x,y,z,j), print("La cantidad de combinaciones posibles de " + str(movimientos) + " es de " + str(x[-1]*4+y[-1]*2+z[-1]*2+j[-1]))
+recorrer()
+print(x,y,z,j), print("La cantidad de combinaciones posibles de " + str(movimientos) + " es de " + str(x[-1]*4+y[-1]*2+z[-1]*2+j[-1]))
